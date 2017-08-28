4361b12c28dd18024df6d060521aa2ec622dd93a
Luís Ferreira <lsferreira169@gmail.com>
2017-08-28 16:44:32 +0100

    Fix app and library link

--- app.cmake
+++ app.cmake
@@ -6,4 +6,6 @@ endif()
 
 add_executable(aurorafw_app_tests_gengine ${APP_TESTS_GENGINE_SOURCE_DIR}/test_gengine.cpp)
 
+target_link_libraries(aurorafw_app_tests_gengine aurorafw-core aurorafw-gengine-core)
+
 set_target_properties(aurorafw_app_tests_gengine PROPERTIES OUTPUT_NAME test_gengine.out)
