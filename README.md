22b3e4a8598a26a8cc480e49689435362e2bd1cc
Luís Ferreira <lsferreira169@gmail.com>
2017-09-03 00:22:49 +0100

    Making several code optimizations and project restructuration

--- README.md
+++ README.md
@@ -12,10 +12,10 @@
 **
 ** This file is part of the Aurora Framework. This framework is free
 ** software; you can redistribute it and/or modify it under the terms of
-** the GNU Lesser General Public License version 3 as published by the
-** Free Software Foundation and appearing in the file LICENSE included in
-** the packaging of this file. Please review the following information to
-** ensure the GNU Lesser General Public License version 3 requirements
-** will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
+** the GNU General Public License version 3 as published by the Free
+** Software Foundation and appearing in the file LICENSE included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU General Public License version 3 requirements will be
+** met: https://www.gnu.org/licenses/gpl-3.0.html.
 ****************************************************************************/
 ```
--- app.cmake
+++ app.cmake
@@ -8,20 +8,20 @@
 #
 # This file is part of the Aurora Framework. This framework is free
 # software; you can redistribute it and/or modify it under the terms of
-# the GNU Lesser General Public License version 3 as published by the
-# Free Software Foundation and appearing in the file LICENSE included in
-# the packaging of this file. Please review the following information to
-# ensure the GNU Lesser General Public License version 3 requirements
-# will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
+# the GNU General Public License version 3 as published by the Free
+# Software Foundation and appearing in the file LICENSE included in the
+# packaging of this file. Please review the following information to
+# ensure the GNU General Public License version 3 requirements will be
+# met: https://www.gnu.org/licenses/gpl-3.0.html.
 
 message(STATUS "Loading tests-gengine app...")
 
 if (NOT CONFIGURED_ONCE)
-	set(APP_TESTS_GENGINE_SOURCE_DIR ${APP_TESTS_GENGINE_DIR}/src)
+	set(AURORAFW_APP_TESTS_GENGINE_SOURCE_DIR ${AURORAFW_APP_TESTS_GENGINE_DIR}/src)
 endif()
 
-add_executable(aurorafw_app_tests_gengine ${APP_TESTS_GENGINE_SOURCE_DIR}/main.cpp)
+add_executable(aurorafw_app_tests_gengine ${AURORAFW_APP_TESTS_GENGINE_SOURCE_DIR}/main.cpp)
 
 target_link_libraries(aurorafw_app_tests_gengine aurorafw-core aurorafw-gengine-core)
 
-set_target_properties(aurorafw_app_tests_gengine PROPERTIES OUTPUT_NAME test_gengine.out)
+set_target_properties(aurorafw_app_tests_gengine PROPERTIES OUTPUT_NAME aurorafw_app_tests_gengine.out)
